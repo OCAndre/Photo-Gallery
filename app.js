c7ae2521c5578099d894f6daa87e4615d5c748e1
Zachary Davis <kicksilver@icloud.com>
2022-09-27 19:45:32 -0400

    Added app.js starter code

---
+++ app.js
@@ -0,0 +1,50 @@
+// DEPENDENCIES
+const express = require('express')
+const methodOverride = require('method-override')
+const { Sequelize } = require('sequelize')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+Sequelize.connect(process.env.PG_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to postgresql: ', process.env.PG_URI) }
+)
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to knockoff Yelp</h1>')
+})
+  
+// Login
+app.get('/login', (req, res) => {
+    res.send()
+})
+
+// Restaurants
+app.get('/localrestaurants', (req, res) => {
+    res.send()
+})
+
+//Topten
+app.get('/topten', (req, res) => {
+    res.send()
+})
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('Live on port', PORT);
+})
