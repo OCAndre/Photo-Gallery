1ed0930065a478d2863fe22d4a6cfbe3c77da9dc
Chrisbick94 <101513319+Chrisbick94@users.noreply.github.com>
2022-09-27 19:38:23 -0400

    added skeleton pages

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../styles/login.css'
+
+function Login() {
+    
+  return (
+    <div>
+        <div className='loginbox' >Login Box Here</div>
+
+        <div className='createacc'>Create Account Here</div>
+
+        
+        
+    </div>
+  )
+}
+
+export default Login
