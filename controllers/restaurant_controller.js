7d14435f79b3ef7a68481234af9c512e8865d9f9
Owen Andre <owencandre@gmail.com>
2022-09-27 20:53:54 -0400

    added basic comments

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -19,6 +19,11 @@ restaurants.get('/:id', async (req, res) => {
         const foundRestaurant = await Restaurant.findOne({
             where: { restaurant_id: req.params.id }
         })
+            .populate('comments')
+            .then(restaurant => {
+                console.log(restaurant.comments)
+                res.render('restaurants/show', { restaurant })
+            })
         res.status(200).json(foundRestaurant)
     } catch (error) {
         res.status(500).json(error)
@@ -70,7 +75,41 @@ restaurants.delete('/:id', async (req, res) => {
     }
 })
 
+// Reviews
 
+// comment
+restaurants.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/restaurant/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+// delete comment
+restaurants.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            res.redirect(`/restaurants/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
 
 // EXPORT
 module.exports = restaurants
