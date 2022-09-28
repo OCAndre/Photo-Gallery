00537240beb469837e3a34a5e20a221119ad39ca
Owen Andre <owencandre@gmail.com>
2022-09-27 20:00:13 -0400

    basic restaurant controller

--- controllers/restaurant_controller.js
+++ controllers/restaurant_controller.js
@@ -13,3 +13,64 @@ restaurants.get('/', async (req, res) => {
     }
 })
 
+// FIND A SPECIFIC restaurant
+restaurants.get('/:id', async (req, res) => {
+    try {
+        const foundRestaurant = await Restaurant.findOne({
+            where: { restaurant_id: req.params.id }
+        })
+        res.status(200).json(foundRestaurant)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A restaurant (Do we need this?)
+restaurants.post('/', async (req, res) => {
+    try {
+        const newRestaurant = await Restaurant.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new restaurant',
+            data: newRestaurant
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE A restaurant (Do we need this?)
+restaurants.put('/:id', async (req, res) => {
+    try {
+        const updatedRestaurant = await Restaurant.update(req.body, {
+            where: {
+                restaurant_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedRestaurant} restaurant(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A restaurant
+restaurants.delete('/:id', async (req, res) => {
+    try {
+        const deletedRestaurant = await Restaurant.destroy({
+            where: {
+                restaurant_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedRestaurant} restaurant(s)`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+// EXPORT
+module.exports = restaurants
